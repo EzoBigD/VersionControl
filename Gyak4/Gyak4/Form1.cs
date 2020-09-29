6dc5788049c29e53c23588fe2cd7aace78e5df90
Ádám Ezzeddine <adam.ezzeddine@stud.uni-corvinus.hu>
2020-09-29 20:45:17 +0200

    5

--- Gyak4/Gyak4/Form1.cs
+++ Gyak4/Gyak4/Form1.cs
@@ -14,8 +14,13 @@ namespace Gyak4
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet;
+
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,5 +30,40 @@ namespace Gyak4
         {
             Flats = context.Flats.ToList();
         }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+        
+
     }
 }
