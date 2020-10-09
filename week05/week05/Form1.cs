76c8ee0e1118d73f880d5184376294696b973d11
Ádám Ezzeddine <adam.ezzeddine@stud.uni-corvinus.hu>
2020-10-09 19:26:48 +0200

    7

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -65,6 +66,29 @@ namespace week05
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = " (.txt)|.txt";
+            sfd.DefaultExt = "txt";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var b in Ticks)
+                {
+                    sw.Write(b.Index);
+                    sw.Write(b.TradingDay);
+                    sw.Write(b.Price);
+                    sw.Write(b.Volume);
+                }
+            }
         }
     }
 }
