5a5eca520a32a51347f140c6bc5c8a277e0cba0f
Ádám Ezzeddine <adam.ezzeddine@stud.uni-corvinus.hu>
2020-10-15 13:26:04 +0200

    7

--- Gyak6/Gyak6/Form1.cs
+++ Gyak6/Gyak6/Form1.cs
@@ -22,13 +22,19 @@ namespace Gyak6
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
@@ -77,6 +83,21 @@ namespace Gyak6
         private void bindingSource1_CurrentChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
