af82d01210d01a3079c21dc92e2551edc71ed8cb
Ádám Ezzeddine <adam.ezzeddine@stud.uni-corvinus.hu>
2020-11-11 13:24:30 +0100

    8.11

--- Gyak8/Gyak8/Form1.cs
+++ Gyak8/Gyak8/Form1.cs
@@ -17,11 +17,17 @@ namespace Gyak8
     {
         private List<Toy> _toys = new List<Toy>();
 
-        public IToyFactory _factory;
+        private Toy _nextToy;
+
+        private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set 
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
         public Form1()
         {
@@ -54,5 +60,24 @@ namespace Gyak8
                 _toys.Remove(oldestBall);
             }
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }       
     }
 }
