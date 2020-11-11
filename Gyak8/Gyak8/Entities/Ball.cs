6447fa705eb27454e76c6e7636723ea8277be8ba
Ádám Ezzeddine <adam.ezzeddine@stud.uni-corvinus.hu>
2020-11-11 12:34:48 +0100

    8.6

--- Gyak8/Gyak8/Entities/Ball.cs
+++ Gyak8/Gyak8/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gyak8.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Windows.Forms;
 
 namespace Gyak8.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
         public Ball()
         {
@@ -23,7 +24,7 @@ namespace Gyak8.Entities
             DrawImage(e.Graphics);
         }
 
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
