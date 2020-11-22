e58a587a8b9e4e2f92322d001696d2da92b5a107
Ádám Ezzeddine <adam.ezzeddine@stud.uni-corvinus.hu>
2020-11-22 19:15:36 +0100

    9.3

---
+++ UnitTestExample/UnitTestExample.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("40b37c11-2c1c-4d50-8876-1c5cacc26643")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UserMaintenance/UnitTestExample/AccountControllerTestFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace UnitTestExample
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+          
+        }
+    }
+}
